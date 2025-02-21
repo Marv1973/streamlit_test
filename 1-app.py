b82fa3958280ce239c7e2ae57131e7e3d7b7afd7
Marten <marv73@gmail.com>
2025-02-21 12:25:54 +0100

    Initial commit

---
+++ 1-app.py
@@ -0,0 +1,52 @@
+# 
+
+from pandas import from_dummies
+import streamlit as st
+
+# load env and raise error if not loaded
+from dotenv import load_dotenv, find_dotenv
+loaded=load_dotenv("C:/Users/marv7/Python/.env")
+if not loaded:
+    raise ValueError("File not loaded")
+
+from langchain_groq import ChatGroq
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import numpy as np
+
+
+st.title("ARC Challenge")
+
+#st.chat_input(placeholder="Hi you")#, *, key=None, max_chars=None, disabled=False, on_submit=None, args=None, kwargs=None)
+
+
+
+user_prompt=st.chat_input(placeholder="Hi you 1" , key=None, max_chars=None, disabled=False, on_submit=None, args=None, kwargs=None)
+
+print(user_prompt)
+
+# %% 
+if user_prompt:
+    message = st.chat_message("assistant")
+    message.write("Hello human")
+    message.bar_chart(np.random.randn(30, 3))
+
+
+# %%
+@st.dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
+
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
